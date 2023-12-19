76992eec5051ac7e7ff917f06a7c832799b1cfdc
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-12-19 12:31:49 +0530

    MyTelivisionAPI

---
+++ MobileApi/Model/MyTelivision.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTelevisionApi.Model
+{
+    public class MyTelivision
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]  
+        public string? Name { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]  
+        public string? color { get; set; }   
+    }
+}
