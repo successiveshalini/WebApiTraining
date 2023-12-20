a1a2651204c8cc348bf6f43b7446725dd4d6e6e4
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-12-20 12:51:55 +0530

    ModelChanges

--- MyWatchApi/Model/WatchDetails.cs
+++ MyWatchApi/Model/WatchDetails.cs
@@ -10,8 +10,32 @@ namespace MyWatchApi.Model
 
         [StringLength(10, MinimumLength = 5)]
         public string? Name { get; set; }
-        [Required]
-        public string? Description { get; set; }
+        [Required]using System.ComponentModel.DataAnnotations;
+
+namespace MyWatchApi.Model
+    {
+        public class WatchDetails
+        {
+            [Key]
+            public int Id { get; set; }
+            [Required]
+            //[Required(ErrorMessage = " ID is required")]
+
+            [StringLength(10, MinimumLength = 5)]
+            public string? Name { get; set; }
+            [Required]
+            public string? Description { get; set; }
+            [Required]
+
+            [Range(0, 999, ErrorMessage = "Out of Range ")]
+            public decimal? Price { get; set; }
+
+            [StringLength(10, MinimumLength = 2)]
+            public string? Color { get; set; }
+        }
+    }
+
+    public string? Description { get; set; }
         [Required]
 
         public float? Price { get; set; }
