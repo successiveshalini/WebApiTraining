4e1bce1602441a9e9a601512c189a5fb91e2eab2
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-12-20 12:40:57 +0530

    CostomValidation

---
+++ MyWatchApi/PriceAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWatchApi
+{
+    public class PriceAttribute : ValidationAttribute
+    {
+        protected override ValidationResult  IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || !(value is decimal))
+            {
+                return new ValidationResult("Invalid price format.");
+            }
+            decimal price = (decimal)value;
+            if (price <= 0)
+            {
+                return new ValidationResult("Price must be greater than zero.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
