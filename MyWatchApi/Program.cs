57a9d1e16660587803935bcee349231b0bcbff2c
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-12-20 18:33:42 +0530

    ChangeCode

--- MyWatchApi/Program.cs
+++ MyWatchApi/Program.cs
@@ -1,3 +1,37 @@
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using MyWatchApi.Data;
+using MyWatchApi.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<WatchDetailsDBContext>(options => options.UseSqlServer(ConnectionString));
+builder.Services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<WatchDetails>());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSingleton<WatchDetailsDBContext>(); 
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
 using Microsoft.EntityFrameworkCore;
 using MyWatchApi.Data;
 
