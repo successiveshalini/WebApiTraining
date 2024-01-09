cdac125ad95f2b93c3eecbbf69001a2fbe6c4201
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2024-01-09 17:28:18 +0530

    DependencyInjectionproject

---
+++ DependencyInjection/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoAPIDI
+{
+    public interface IEmployee
+    { 
+        public string? Name { get; set; }
+        public string Description { get; set; } 
+        public string? Address { get; set; } 
+        public int Age { get; set; }    
+    }
+    public class Employee : IEmployee
+    {
+        public string? Name { get; set; }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? Address { get; set; }
+        public int Age { get; set; }
+    }
+}
